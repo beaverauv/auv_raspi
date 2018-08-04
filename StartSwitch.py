96c31cb872133a3af334cc154247bb3d10725664
Seth <isaacson17@bcdschool.org>
2018-08-03 18:39:32 -0700

    some old dude wrote simple code for the youngins

---
+++ StartSwitch.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Bool
+# import RPi.GPIO as GPIO
+
+def talker():
+	rospy.init_node('StartNode', anonymous=True)
+	pub = rospy.Publisher('start', Bool, queue_size=0)
+	rate = rospy.Rate(10) # 10hz
+
+	runOnce = 0;
+	while not rospy.is_shutdown():
+		if True:
+			if(runOnce == 0):
+				rospy.loginfo("Starting Mission")
+				runOnce = 1
+			pub.publish(True)
+		else:
+			pub.publish(False)
+
+		rate.sleep()
+
+
+if __name__ == '__main__':
+	# GPIO.setmode(GPIO.BCM)
+	# GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+	try:
+		talker()
+	except rospy.ROSInterruptException:
+		pass
